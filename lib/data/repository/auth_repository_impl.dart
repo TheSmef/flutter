166d6528691c53697c3281f61e2bda2fe62072eb
Соколов Александр Анатольевич <sokolov19868@gmail.com>
2022-11-12 21:01:57 +0300

    PR3

--- lib/data/repository/auth_repository_impl.dart
+++ lib/data/repository/auth_repository_impl.dart
@@ -37,20 +37,29 @@ class AuthRepositoryImlp implements AuthRepositories {
 
       return Right(RoleEnum.values[(user.first['id_role'] as int) - 1]);
     } on DatabaseException catch (e) {
-      const Left("Непредвиденная ошибка, попробуйте позже");
+      const Left("Произошла неизвестная ошибка, попробуйте позже");
     }
-    return const Left("Непредвиденная ошибка, попробуйте позже");
+    return const Left("Произошла неизвестная ошибка, попробуйте позже");
   }
 
   @override
   Future<Either<String, bool>> signUp(String login, String password, String lastname, String firstname, String otch) async {
     try {
+      var user = await _db.query(
+        table,
+        columns: ['login', 'password', 'id_role'],
+        where: 'login = ?',
+        whereArgs: [login],
+      );
+      if (user.isNotEmpty) {
+        return const Left("Данный логин уже занят другим пользователем");
+      }
       int id = 0;
       id = await _db.insert(
         table,
         User(
           login: login,
-          password: password,
+          password: Crypto.crypto(password),
           id_role: RoleEnum.user.id,
         ).toMap(),
       );
